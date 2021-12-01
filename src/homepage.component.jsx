76f246342c8707680b67bb1c5a9a744266b9f26c
Kenan Haskić <kenan.haskic@green-river-media.com>
2021-12-01 17:07:25 +0100

    new project

---
+++ src/homepage.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './homepage.styles.scss';
+
+const Homepage = () =>(
+
+<div className="homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitles">Shop now</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+);
+
+
+export default Homepage;
