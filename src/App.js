1f9a9669d77a729ca517549cf4d8cf0d237282f2
devkeno <kenan.haskic@green-river-media.com>
2021-12-12 23:26:55 +0100

    Navigation - react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import Homepage from './pages/homepage/homepage.component'; 
+
+const HatsPage= props =>{
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  )
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+ <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/shop/hats">
+            <HatsPage />
+          </Route>
+          <Route path="*">
+          <Homepage />
+          </Route>
+       </Switch>
     </div>
   );
 }
