39f7116034de658e7c785a6cb3cb21a8bb0efa3c
devkeno <kenan.haskic@green-river-media.com>
2021-12-14 16:29:37 +0100

    header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'; 
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div className="App">
+    <Header />
  <Switch>
           <Route exact path="/">
             <Homepage />
